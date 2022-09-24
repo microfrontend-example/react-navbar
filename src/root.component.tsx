1fd87ca2029253fb4802d8dd8ddd1a0961c03acf
Carlos Morales <dev.carlos.morales@gmail.com>
2022-09-24 14:28:17 -0500

    eslint changes

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,52 @@
+import { navigateToUrl } from 'single-spa';
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <nav
+      style={{
+        height: '80px',
+        background: 'var(--dark-grayish-blue)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <ul
+        style={{
+          padding: 0,
+          margin: 0,
+          listStyle: 'none',
+          display: 'flex',
+          gap: '2rem',
+          fontWeight: 'bold',
+          color: 'var(--white)',
+        }}
+      >
+        <li>
+          <a
+            href="/"
+            style={{ cursor: 'pointer', color: 'var(--white)' }}
+            onClick={(event) => {
+              event.preventDefault();
+              navigateToUrl('/');
+            }}
+          >
+            React list
+          </a>
+        </li>
+        <li>
+          <a
+            href="/form"
+            style={{ cursor: 'pointer', color: 'var(--white)' }}
+            onClick={(event) => {
+              event.preventDefault();
+              navigateToUrl('/form');
+            }}
+          >
+            Angular form
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const { merge } = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+const { merge } = require('webpack-merge');
+const singleSpaDefaults = require('webpack-config-single-spa-react-ts');
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
-    orgName: "throwjs",
-    projectName: "navbar",
+    orgName: 'throwjs',
+    projectName: 'navbar',
     webpackConfigEnv,
     argv,
   });
