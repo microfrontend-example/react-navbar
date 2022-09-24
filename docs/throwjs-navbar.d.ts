639a559f1dd27d4d16c72f043ea48803576c5fe6
Carlos Morales <dev.carlos.morales@gmail.com>
2022-09-24 14:28:33 -0500

    feat: add docs

---
+++ docs/throwjs-navbar.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="single-spa" />
+export declare const bootstrap: import('single-spa').LifeCycleFn<any>,
+  mount: import('single-spa').LifeCycleFn<any>,
+  unmount: import('single-spa').LifeCycleFn<any>;
